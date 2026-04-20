9f8b67e854615dd75f32b39c7ff573bda3cd1300
Connor <your_email@example.com>
2026-04-19 23:59:51 -0700

    added a new helper for the perform_dijkstras algorithm named find_immediate_neighbors. rethinking how i want to approach dijkstras algorithm implementation.

--- Graph.cpp
+++ Graph.cpp
@@ -1,5 +1,6 @@
 #include "Graph.h"
 #include <iostream>
+#include <climits>
 
 Graph::Graph(int num_verticies)
 {
@@ -15,11 +16,11 @@ Graph::Graph(int num_verticies)
 
 void Graph::update_adjacency_matrix(int vertex1, int vertex2, int weight)
 {
-    int x = vertex1 - 1;
-    int y = vertex2 - 1;
+    int v1 = vertex1 - 1;
+    int v2 = vertex2 - 1;
 
-    adjacency_matrix[x * this->num_verticies + y] = weight;
-    adjacency_matrix[y * this->num_verticies + x] = weight;
+    adjacency_matrix[v1 * this->num_verticies + v2] = weight;
+    adjacency_matrix[v2 * this->num_verticies + v1] = weight;
 }
 
 int *Graph::find_odd_degrees()
@@ -55,8 +56,56 @@ int *Graph::find_odd_degrees()
     return odd_degrees;
 }
 
-int Graph::*perform_dijkstras(int starting_vertex)
+// ********** LAST METHOD TO UPLOAD **********
+int *Graph::perform_dijkstras(int starting_vertex)
 {
+    // int num_verticies = this->num_verticies;
+    // int *weights = new int[num_verticies + 1];
+
+    // // initialize the first element as the number of verticies stored
+    // weights[0] = num_verticies;
+
+    // // initialize all weights to +inf
+    // for (int i = 0; i < num_verticies; i++)
+    // {
+    //     weights[i] = INT_MAX;
+    // }
+
+    // // the weight from starting_vertex to starting_vertex will always be 0
+    // weights[starting_vertex] = 0;
+
+    // int *immediate_neighbors = find_immediate_neighbors(starting_vertex);
+    // int neighbor_count = immediate_neighbors[0];
+
+    // // if the immediate neighbor's access distance is less
+    // for (int i = 1; i <= neighbor_count; i++)
+    // {
+    //     int position_in_matrix = ((starting_vertex - 1) * (this->num_verticies)) + (immediate_neighbors[i] - 1);
+    //     if (adjacency_matrix[position_in_matrix] < weights[immediate_neighbors[i]])
+    //     {
+    //         weights[immediate_neighbors[i]] = adjacency_matrix[position_in_matrix];
+    //     }
+    // }
+}
+
+int *Graph::find_immediate_neighbors(int source_vertex)
+{
+    int *neighbors = new int[this->num_verticies];
+    int neighbor_count = 0;
+
+    int offset = (source_vertex - 1) * this->num_verticies;
+    int end_of_row = offset + this->num_verticies;
+
+    for (int i = offset; i < end_of_row; i++)
+    {
+        if (this->adjacency_matrix[i] != 0)
+        {
+            neighbors[neighbor_count + 1] = (i % this->num_verticies) + 1;
+            neighbor_count += 1;
+        }
+    }
+    neighbors[0] = neighbor_count;
+    return neighbors;
 }
 
 void Graph::print_adjacency_matrix()
--- Graph.h
+++ Graph.h
@@ -2,7 +2,6 @@ class Graph
 {
 private:
     int num_verticies;
-    int num_edges;
     int size_of_adjacency_matrix;
     int *adjacency_matrix;
 
@@ -12,6 +11,7 @@ public:
     void update_adjacency_matrix(int vertex1, int vertex2, int weight);
     int *find_odd_degrees();
     int *perform_dijkstras(int vertex);
+    int *find_immediate_neighbors(int source_vertex);
 
     void print_adjacency_matrix();
     void print_odd_degrees(int *odd_degrees);
